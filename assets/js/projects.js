cc37b8a431998d9ece6345da1c68e0950651e217
rroyalty <rroyalty@gmail.com>
2021-02-14 16:36:39 -0500

    st

---
+++ assets/js/projects.js
@@ -0,0 +1,23 @@
+const projectList = 
+
+{
+    projects:
+    [
+        {   
+            name: "Easy Recipeazy",             
+            description: "A website that takes ingredients you have in your house and offers you recipes to make with them!", 
+            domain: "https://tonipow3ll.github.io/drinks-and-dinner/",
+            github: "https://github.com/tonipow3ll/drinks-and-dinner",
+            imgSrc: "https://raw.githubusercontent.com/tonipow3ll/drinks-and-dinner/main/images/mealdrink.png",
+            icon: "fas fa-utensils",
+        },
+        {   
+            name: "Code Quiz",             
+            description: "A timed, multiple choice, pop culture quiz that saves your highscores!", 
+            domain: "https://rroyalty.github.io/code-quiz/",
+            github: "https://github.com/rroyalty/code-quiz/",
+            imgSrc: "https://github.com/rroyalty/code-quiz/blob/main/assets/images/codeQuizQuestionsScreen.jpg",
+            icon: "fas fa-question",
+        },
+    ]
+};
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,55 @@
 $(document).ready(function() {
 
-    projectList.projects.forEach((element, index) => {
-        rowChildren(element.name, element.icon, index);
-    })
+    projectList.projects.forEach((element, index, array) => {
+        let arrLength = array.length;
+        rowChildren(element.name, element.icon, index, arrLength);
+    });
 
-    function rowChildren(text, icon, index) {
+    populateProject(projectList.projects[0])
 
-        const title = `<a class="panel-block projectItem-border indian-khaki-font">
-                        <span class="panel-icon indian-khaki-font">
-                            <i class="${icon}" aria-hidden="true"></i>
-                        </span>${text}<a>`;
+    function populateProject(element) {
+        console.log(element)
+
+        // `<div class="content">
+        //     <h1 class="indian-khaki-font center-text">${element}</h1>
+        // </div>
+        // <div class="card-content columns">
+        //     <div class="column">
+        //         <img src="https://raw.githubusercontent.com/tonipow3ll/drinks-and-dinner/main/images/mealdrink.png" alt="${element} Image">
+        //     </div>
+        //     <div class = "column">
+        //         <p>A website that takes ingredients you have in your house and offers you recipes to make with them!</p>
+        //         <div class="columns">
+        //         <div class="column center-text">
+        //             <a class="center-text" href="https://tonipow3ll.github.io/drinks-and-dinner/" alt="${element} URL">Check it out!</a>
+        //         </div>
+        //         <div class="column center-text">
+        //             <a href="https://github.com/tonipow3ll/drinks-and-dinner" alt="${element} Repository">Repository</a>
+        //         </div>
+        //         </div>
+        //     </div>
+        // </div>`
+
+    };
+
+    function rowChildren(text, icon, index, length) {
+        let classAdd = "indian-khaki-font"
+        let iconAdd = " indian-khaki-font"
+
+        switch(index) {
+            case 0:
+                iconAdd = "matisse-font";
+                classAdd = "matisse-font activeProject";
+            break;
+            case length - 1:
+                classAdd = "indian-khaki-font lastItem-border";
+            break;
+        }
+
+        const title = `<a class="panel-block projectItem-border ${classAdd}">
+                        <span class="panel-icon">
+                            <i class="${icon} ${iconAdd}" aria-hidden="true"></i>
+                        </span>${text}</a>`;
         $("#projectList").append(title);
     };
 
